44e7559296871f2ede421b3ac570834bf255f2e9
kdylsky <kdy9kdy@gmail.com>
2023-03-05 14:58:22 +0900

    feat: flask-form을 이용한 유저 form 정의

--- googlekap/forms/auth_form.py
+++ googlekap/forms/auth_form.py
@@ -1,7 +1,14 @@
 from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, EqualTo
 
 class LoginFrom(FlaskForm):
-    pass
+    # 프론트에 표시될 라벨과 유효성검사
+    user_id = StringField("User Id", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
 
-class RegisterFrom(FlaskForm):
-    pass
+
+class RegisterFrom(LoginFrom):
+    user_name = StringField("User Name", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired(), EqualTo("repassword", message="Password mush match!!")])
+    repassword = PasswordField("RePassword", validators=[DataRequired()])
