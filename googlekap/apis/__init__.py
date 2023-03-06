c45a7d2b1c63bae294dff7ffdd654d0fe1dde619
kdylsky <kdy9kdy@gmail.com>
2023-03-06 18:06:51 +0900

    feat: 세션을 이용한 로그인 확인

--- googlekap/apis/__init__.py
+++ googlekap/apis/__init__.py
@@ -1,14 +1,30 @@
-from flask import Blueprint
+from flask import Blueprint, g, abort, current_app
 from flask_restx import Api
 from .user import ns as UserNamespace
+from functools import wraps
 
 blueprint = Blueprint("api", __name__, url_prefix="/api")
+
+
+# 데코레이터
+# 함수를 인자로 받는다. 인자로 받은 함수를 func 실행시켜준다.
+# 실행시켜준 함수를 리턴한다.
+def check_session(func):
+    @wraps(func)
+    def __wrapper(*args, **kwargs):
+        if not g.user:
+            abort(401)
+        return func(*args, **kwargs)
+
+    return __wrapper
+
 
 api = Api(
     blueprint,
     title="Google Kap API",
     version="1.0",
     doc="/doc",
+    decorators=[check_session],
     description="Weloce my API docs",
 )
 
--- googlekap/routes/base_route.py
+++ googlekap/routes/base_route.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, g, redirect, url_for
 
 NAME = "base"
 # name_space, __name__
@@ -7,4 +7,7 @@ bp = Blueprint(NAME, __name__)
 
 @bp.route("/")
 def index():
+    """g 컨텍스트를 이용해서"""
+    if not g.user:
+        return redirect(url_for("auth.login"))
     return render_template("index.html")
