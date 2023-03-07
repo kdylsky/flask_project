954a29538228e3a441046fad132a17beb8629199
kdylsky <kdy9kdy@gmail.com>
2023-03-07 10:11:51 +0900

    test: 메모 tdd 테스트케이스 작성

--- googlekap/tests/conftest.py
+++ googlekap/tests/conftest.py
@@ -6,6 +6,7 @@ sys.path.append(".")
 from googlekap.configs import TestingConfig
 from googlekap import create_app, db
 from googlekap.models.user import User as UserModel
+from googlekap.models.memo import Memo as MemoModel
 import pytest
 import os
 
@@ -13,17 +14,35 @@ import os
 # 더미 유저 데이터 만들기
 @pytest.fixture(scope="session")
 def user_data():
-    yield dict(user_id="test_id", user_name="test_name", password="test_password")
+    yield dict(
+        user_id="test_id",
+        user_name="test_name",
+        password="test_password",
+    )
+
+
+# 더미 데이터 메모 만들기
+@pytest.fixture(scope="session")
+def memo_data():
+    yield dict(
+        title="tite",
+        content="content",
+    )
 
 
 @pytest.fixture(scope="session")
-def app(user_data):
+def app(user_data, memo_data):
     app = create_app(TestingConfig)
     # 이 컨텍스트 안에서 데이터베이스 초기화
     with app.app_context():
         db.drop_all()
         db.create_all()  # flask db가 업그레이드 된 것 처럼 동작한다.
-        db.session.add(UserModel(**user_data))
+        user = UserModel(**user_data)
+        db.session.add(user)
+        # db에 commit하는 것 처럼 flush를 해주어야지만 user.id를 받아올 수 있다.
+        db.session.flush()
+        memo_data["user_id"] = user.id
+        db.session.add(MemoModel(**memo_data))
         db.session.commit()
         yield app
         # 불필요한 디비 정리
